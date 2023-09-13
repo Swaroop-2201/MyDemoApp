ea187401e4122ecbcead5ce9e15ad76979d91412
Swaroop-2201 <swaroop.devp@gmail.com>
2023-09-13 13:53:33 +0530

    Dashboard added

---
+++ S3Project'/app/src/main/java/com/example/s3project/Dashboard/student_dashboard.java
@@ -0,0 +1,16 @@
+package com.example.s3project.Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.s3project.R;
+
+public class student_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_dashboard);
+    }
+}
---
+++ S3Project'/app/src/main/java/com/example/s3project/Dashboard/teacher_dashboard.java
@@ -0,0 +1,16 @@
+package com.example.s3project.Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.s3project.R;
+
+public class teacher_dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_dashboard);
+    }
+}
